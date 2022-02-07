6789db93a969c5134ff82c929dc2a45d0fc571c7
Vasil Simeonov <vasko3107@gmail.com>
2022-02-07 14:29:59 +0200

    adding starting project

---
+++ startingProject/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/img/tuco.jpg" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>vasilsimeonov</h1></div>
+            <div class="d-flex">
+                <div class="pe-3"><strong>999</strong> posts</div>
+                <div class="pe-3"><strong>999</strong> followers</div>
+                <div class="pe-3"><strong>999</strong> following</div>
+            </div>
+            <div class="pt-4"><strong>befit.com</strong></div>
+            <div><a href="#">befit.com</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="/img/bmw.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/img/bmw.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/img/bmw.jpg" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
