626ca13e0618c972f915f5a838c86ac1440ea57a
Vasil Simeonov <vasko3107@gmail.com>
2022-02-08 14:59:53 +0200

    0.1 update

--- startingProject/resources/views/home.blade.php
+++ startingProject/resources/views/home.blade.php
@@ -7,25 +7,26 @@
             <img src="/img/tuco.jpg" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>vasilsimeonov</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pe-3"><strong>999</strong> posts</div>
                 <div class="pe-3"><strong>999</strong> followers</div>
                 <div class="pe-3"><strong>999</strong> following</div>
             </div>
-            <div class="pt-4"><strong>befit.com</strong></div>
-            <div><a href="#">befit.com</a></div>
+            <div class="pt-4"><strong>{{ $user->profile->title }}</strong></div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
         <div class="col-4">
-            <img src="/img/bmw.jpg" class="w-100">
+            <img src="/img/bmw_e39.jpg" class="w-100">
         </div>
         <div class="col-4">
-            <img src="/img/bmw.jpg" class="w-100">
+            <img src="/img/bmw_e39.jpg" class="w-100">
         </div>
         <div class="col-4">
-            <img src="/img/bmw.jpg" class="w-100">
+            <img src="/img/bmw_e39.jpg" class="w-100">
         </div>
     </div>
 </div>
